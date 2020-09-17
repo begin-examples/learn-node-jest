dc3b8886fdeb1156d19c84378b2cb3d3cc44be67
Brian LeRoux <b@brian.io>
2020-09-16 18:39:59 -0700

    upgrade to sandbox 2.x and httpapis compat

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,6 +1,7 @@
 // learn more about HTTP functions here: https://arc.codes/primitives/http
 exports.handler = async function http (req) {
   return {
+    statusCode: 200,
     headers: {'content-type': 'text/html; charset=utf8'},
     body: `
 <!DOCTYPE html>
